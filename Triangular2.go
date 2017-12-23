d2700cad4a6ba8417c9d1bf877ba10591b6ff8e5
SUbbu <subbu05@users.noreply.github.com>
2017-12-22 16:20:25 -0800

    Create Triangular2.go

---
+++ Triangular2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	n := 5
+  //Using the formuala instead recursive or iterative  function
+	result := (int) ((n * n) + n) / 2
+
+	fmt.Println(result)
+
+}
